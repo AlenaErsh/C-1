fc89ac4cf088be7bc8b43fe4f7b3113e12387d30
Alena <nikolalena98@mail.ru>
2017-10-11 11:26:56 +0300

    first commit

---
+++ _15_1/_15_1.cpp
@@ -0,0 +1,155 @@
+// Ex15_lab1.cpp: определяет точку входа для консольного приложения.
+//
+/*Дана матрица содержащая как положительные, так и отрицательные элементы. 
+Удалить все столбцы, содержащие только положительные элементы*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include "iomanip" 
+
+using namespace std;
+
+typedef int TElem;
+
+void Ex();
+void Task();
+void PrintMatr(int str, int col);
+bool Corr_Dann();
+void Razm();
+void InputMatr();
+void Rand();
+void Menu();
+
+TElem mas[500][500];
+int n = 0, m = 0;
+bool ex ;
+
+void Ex() {
+	short answ;
+		cout <<"Выберите дальнейшее действие:" << endl;
+		cout << "1. Ввести матрицу заново" << endl << "2. Закончить работу программы" << endl;
+		cin >> answ;
+		switch (answ)
+		{
+		case 1:Menu(); break;
+		case 2:return;
+		default:
+			cout << "Ошибка. Введите корректный номер пункта меню." << endl;
+			Ex(); 
+			break;
+		}
+}
+
+
+void Task() {
+	int col = 0;
+	bool fl;
+	int j;
+	for ( j = 0; j < m; j++) {
+		fl = true;
+		for (int i = 0; (i < n)&&(fl); i++) {
+			fl=(mas[i][j] > 0) ? true : false;
+		}
+		if (fl) {
+			col++;
+			continue;
+		}
+		for (int i = 0; i < n; i++) {
+			mas[i][j - col] = mas[i][j];
+		}
+	}
+	if (col == m) {
+		cout << "Ошибка. Матрица состоит только из положительных чисел." << endl;
+		Ex();
+		return;
+	}
+	cout<<endl;
+	cout << "Решение задачи:" << endl;
+	PrintMatr(n, j-col);	
+}
+
+void PrintMatr(int str, int col){
+	cout << "------------------" << endl;
+	cout << "Матрица: " << endl;
+	for (int i = 0; i < str; i++){
+		for (int j = 0; j < col; j++)
+			cout << setw(4) << setprecision(2) << mas[i][j] << ' ';
+		cout << endl;
+	}
+	if (ex) {
+		ex = false;
+		Task();
+	}
+}
+
+bool Corr_Dann(){
+	if (n < 1){
+		cout << "Некорректные данные. Введите число больше 0" << endl;
+		return false;
+	}
+	return true;
+}
+
+void Razm (){
+	do{
+		cout << "Введите N(количество строк матрицы)= "; cin >> n;
+	} while (!Corr_Dann());
+	do {
+		cout << "Введите M(количество столбцов матрицы)= "; cin >> m;
+	} while (!Corr_Dann());
+}
+
+void InputMatr(){
+	bool fl;
+	cout << "Введите матрицу целых чисел A размером " << n << " x " << m << endl;
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++) {
+				cout << "[" << i + 1 << "," << j + 1 << "]="; cin >> mas[i][j];
+		}
+	}
+	ex = true;
+	PrintMatr(n,m);
+}
+
+void Rand(){
+	srand(time(NULL));
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++) {
+			mas[i][j] = rand() % 240 - 20;
+		}
+	}
+	ex = true;
+	PrintMatr(n,m);
+}
+
+void Menu(){
+	short answ;
+		cout << "Выберите подходящий пункт меню:" << endl;
+		cout << "1. Заполнить матрицу вручную;" << endl;
+		cout << "2. Заполнить матрицу рандомом." << endl;
+		cin >> answ;
+		switch (answ) {
+		case 1: 
+			InputMatr(); 
+			break;
+		case 2: 
+			Rand();
+			break;
+		default:
+			cout << "Ошибка. Введите корректный номер пункта меню" << endl;
+			Menu();
+			break;
+		}
+}
+
+
+int main(){
+	int n=0, m=0;
+	setlocale(LC_ALL, "RUS");
+	Razm();
+	Menu();
+	system("pause");
+	return 0;
+}
+
